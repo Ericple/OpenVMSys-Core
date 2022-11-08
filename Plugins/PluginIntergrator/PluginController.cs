7d213163f4a91f94f8dd74bfc939ba913388ee2a
Ericple <dev@peercat.cn>
2022-11-08 12:57:10 +0800

    Added plugin support

---
+++ Plugins/PluginIntergrator/PluginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Reflection;
+using OpenVMSys_Console.Module;
+
+namespace OpenVMSys_Core.Plugins.PluginIntergrator
+{
+    [ApiController]
+    [Route("api")]
+    public class PluginController : ControllerBase
+    {
+        private PluginManager pluginManager = new();
+        private static readonly OpenVMSysPluginCore openVMSysPluginService = new();
+        private static readonly ArrayList pluginList = openVMSysPluginService.LoadAllPlugins();
+        [HttpGet("{apiName}")]
+        public ActionResult<object> Get(string apiName, string data)
+        {
+            foreach(var plugin in pluginList)
+            {
+                if (apiName == plugin.GetType().Name && pluginManager.IsEnabled(apiName))
+                {
+                    MethodInfo OnGet=plugin.GetType().GetMethod("OnGet");
+                    object returnValue=OnGet.Invoke(plugin, new object[] {data});
+                    return returnValue;
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
+using OpenVMSys_Core.Plugins;
+using System.Reflection;
+
+OpenVMSysPluginCore openVMSysPluginCore = new();
+var pluginList = openVMSysPluginCore.LoadAllPlugins();
+foreach (var plugin in pluginList)
+{
+    Type pluginType = plugin.GetType();
+    Console.WriteLine("Loading {0}...", pluginType.Name);
+    MethodInfo OnServiceStart = pluginType.GetMethod("OnServiceStart");
+    object returnValue = OnServiceStart.Invoke(plugin, null);
+    Console.WriteLine(returnValue.ToString());
+}
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
